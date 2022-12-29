5bfc3abc723f2168a725f4b8eb7dd98be8b468fe
unknown <Student@IT-STULOAN-1550.csun.edu>
2022-12-29 14:07:51 -0800

    Initialization

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
-reportWebVitals(sendToVercelAnalytics);
+
